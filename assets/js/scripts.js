ad9ee8c6fd04d6c7d64eb89c1f2142d7f54960c5
Daria Stefanovska <daria.s@fewgoodgeeks.com>
2021-08-06 15:55:49 +0200

    Changes on homepage (added slider - not done, added SciDesign part, added What We Do part

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider', {
+        perPage    : 3,
+        gap: 1
+    } ).mount();
+} );
---
+++ projects.php
@@ -0,0 +1,26 @@
+<section class="projects-slider-section">
+    <div id="image-slider" class="splide">
+        <div class="splide__track">
+            <ul class="splide__list">
+                <li class="splide__slide">
+                    <img src="images/background_images/slider1.jpg">
+                </li>
+                <li class="splide__slide">
+                    <img src="https://via.placeholder.com/450">
+                </li>
+                <li class="splide__slide">
+                    <img src="https://via.placeholder.com/450">
+                </li>
+                <li class="splide__slide">
+                    <img src="https://via.placeholder.com/450">
+                </li>
+                <li class="splide__slide">
+                    <img src="https://via.placeholder.com/450">
+                </li>
+                <li class="splide__slide">
+                    <img src="https://via.placeholder.com/450">
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
