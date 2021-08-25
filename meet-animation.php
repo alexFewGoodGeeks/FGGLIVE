b449bea112cc77629dc554f5c3833b6a0d882434
Daria Stefanovska <daria.s@fewgoodgeeks.com>
2021-08-25 16:38:13 +0200

    Tree animation changes and work added

--- meet-animation.php
+++ meet-animation.php
@@ -1,13 +1,25 @@
-<div class="row tree-animation">
-    <div class="col-md-4">
+<div class="tree-animation">
+    <div class="col-md-4 left-part-tree">
         <hr style="height:4px;" class="left-line">
         <div class="circle"></div>
+        <div class="vl"></div>
+        <div class="vl-rotate"></div>
+        <div class="circle2"></div>
     </div>
     <div class="col-md-4">
-        <div class="tree-animation" id="canvas">
+        <div class="tree-animation">
             <img src="assets/images/meetUsLogo.png" alt="" width="350" height="150" class="animation-img">
         </div>
+        <div class="vl"></div>
     </div>
-    <div class="col-md-4"></div>
+    <div class="col-md-4 right-part-tree">
+        <hr style="height:4px;" class="right-line">
+        <div class="circle"></div>
+        <div class="vl"></div>
+        <div class="vl-rotate"></div>
+        <div class="circle2"></div>
+    </div>
 </div>
+
+
 
